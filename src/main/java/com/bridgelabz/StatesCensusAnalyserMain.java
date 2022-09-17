525fd20ffdc243314844f6b760aed25f5c0cb3ae
mehulbhange <bhangemehul@gmail.com>
2022-09-17 19:21:45 +0530

    ADDED:[Mehul] added welcome message in main

---
+++ src/main/java/com/bridgelabz/StatesCensusAnalyserMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class StatesCensusAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to states census Analyser program");
+    }
+}
